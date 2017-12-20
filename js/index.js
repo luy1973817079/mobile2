1f4422befb21cab50033636b7420b020514fd5f4
luying1973817079 <1973817079@qq.com>
2017-12-20 10:29:15 +0800

    page

---
+++ js/index.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+    function pgScale(){
+        var deviceWidth = document.documentElement.clientWidth;
+        if(deviceWidth > 750) deviceWidth = 750;
+        document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
+    }
+    pgScale();
+    window.onresize=pgScale;
+    var wow = new WOW({
+        boxClass: 'wow',
+        animateClass: 'animated',
+        offset: 0,
+        mobile: true,
+        live: true
+    });
+    wow.init();
+
+}
+
